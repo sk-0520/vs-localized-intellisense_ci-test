cd000d44248ea2ba25f95b7a947457c684fcffbe
sk <content.type.text.net@gmail.com>
2024-02-18 21:33:17 +0900

    Window にべた書きしようかとも思ったけど一応ね

---
+++ src/vs-localized-intellisense.app/VsLocalizedIntellisense/Models/BindModelBase.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsLocalizedIntellisense.Models
+{
+    /// <summary>
+    /// MVVM で使用するモデル基底。
+    /// </summary>
+    public abstract class BindModelBase : NotifyPropertyBase
+    { }
+}
--- src/vs-localized-intellisense.app/VsLocalizedIntellisense/Models/NotifyModelBase.cs
+++ src/vs-localized-intellisense.app/VsLocalizedIntellisense/Models/NotifyModelBase.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,7 +26,7 @@ namespace VsLocalizedIntellisense.Models
 
         #region function
 
-        protected void OnPropertyChanged(string propertyName)
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
