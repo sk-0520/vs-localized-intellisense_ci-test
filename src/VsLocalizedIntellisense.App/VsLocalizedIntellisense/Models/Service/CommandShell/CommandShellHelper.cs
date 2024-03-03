74afe60990ad2a60f8f4a7624d220c88ecb41def
sk <content.type.text.net@gmail.com>
2024-03-03 23:21:46 +0900

    後でエスケープします！

---
+++ src/VsLocalizedIntellisense.App/VsLocalizedIntellisense/Models/Service/CommandShell/CommandShellHelper.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsLocalizedIntellisense.Models.Service.CommandShell
+{
+    public static class CommandShellHelper
+    {
+        #region function
+
+        public static string Escape(string raw)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
